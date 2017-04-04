b75b2d987058765143e9f2734f84a347f1fd86ad
zhangfan <baoxue113@163.com>
2017-04-04 21:16:30 +0800

    完成示例8（第一章）

--- Ch02/_example7_.py
+++ Ch02/_example7_.py
@@ -1,5 +1,5 @@
 # coding=utf-8
 
-# 归一化处理数据
+
 import kNN
 kNN.classifyPerson()
---
+++ Ch02/_example8_.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+import kNN
+testVector = kNN.img2vector('digits/testDigits/0_13.txt')
+print testVector[0,0:31]
