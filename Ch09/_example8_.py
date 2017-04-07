9358e2b49815e1d660d081958e130d996226d62c
zhangfan <baoxue113@163.com>
2017-04-07 20:50:46 +0800

    完成示例8（第九章）

---
+++ Ch09/_example8_.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# 使用Python的Tkinter库创建GUI
+import regTrees
+from numpy import *
+from Tkinter import *
+root = Tk()
+myLabel = Label(root, text="Hello World")
+myLabel.grid()
+root.mainloop()
+
