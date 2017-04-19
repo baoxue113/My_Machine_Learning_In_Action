1abb68ba6c6e1a914b3de570dbee6062d5f0d359
zhangfan <baoxue113@163.com>
2017-04-20 00:07:07 +0800

    完成示例7的解释（第四章） 使用朴素贝叶斯分类器从个人广告中获取区域倾向 => 测试feedparser

---
+++ Ch04/_example7_.py
@@ -0,0 +1,13 @@
+# # coding=utf-8
+# # 使用朴素贝叶斯分类器从个人广告中获取区域倾向 => 测试feedparser
+# import bayes
+# import feedparser
+# ny = feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+# sf = feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+# vocabList,pSF,pNY = bayes.localWords(ny,sf)
+# vocabList,pSF,pNY = bayes.localWords(ny,sf)
+#
+#
+#
+#
+#
